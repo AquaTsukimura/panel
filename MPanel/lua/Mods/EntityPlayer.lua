412ec889a0fa65dfbe2ee5f505f9fa899baf1ddd
Aqua Tsukimura <165631517+AquaTsukimura@users.noreply.github.com>
2026-08-17 16:02:53 +0500

    Delete MPanel/lua directory

--- MPanel/lua/Mods/EntityPlayer.lua
+++
@@ -1,4 +0,0 @@
-function EntityPlayer:Sync()
-  self:setVelocity({x = 0, y = 0, z = 0})
-  self:moveEntity({x = 0, y = 0, z = 0})
-end
--- MPanel/lua/Mods/FixedVec3.lua
+++
@@ -1,4 +0,0 @@
-LuaTimer:scheduleTimer(function()
-    VectorUtil.ZERO = {x = 0, y = 0, z = 0}
-  end, 5000, -1)
-VectorUtil.ZERO = {x = 0, y = 0, z = 0}
